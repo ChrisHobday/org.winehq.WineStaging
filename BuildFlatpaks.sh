14ac3fd83db350279e0869321beafa9b4453ba5d
Chris Hobday <Hobday.Chris@ProtonMail.com>
2024-11-20 23:50:44 -0500

    Update Wine and Wine Staging to 9.20

--- BuildFlatpaks.sh
+++ BuildFlatpaks.sh
@@ -3,6 +3,6 @@
 flatpak run org.flatpak.Builder --force-clean --repo=repo build-dir org.winehq.WineStaging.yml
 
 # Build Flatpak bundles for WineStaging. WineStaging.mono and WineStaging.gecko
-flatpak build-bundle repo WineStaging9.19.flatpak org.winehq.WineStaging stable-23.08
+flatpak build-bundle repo WineStaging9.20.flatpak org.winehq.WineStaging stable-23.08
 flatpak build-bundle --runtime repo WineStaging.mono9.3.0.flatpak org.winehq.WineStaging.mono stable-23.08
 flatpak build-bundle --runtime repo WineStaging.gecko2.47.4.flatpak org.winehq.WineStaging.gecko stable-23.08
